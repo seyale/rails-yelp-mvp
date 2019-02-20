42cb3722883a08856e08258c208993aad9555592
Alexander Seyss <a@seyss.at>
2019-02-20 15:08:12 +0000

    models, controllers, routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :restaurants
+  end
+
+  # Routing Restaurants User
+  resources :restaurants, only: [:index, :new, :create, :show]
+
+  # Routing Reviews User
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Lius',
+    address:      '1180, Wien',
+    phone_number: '+43 12 12 12',
+    category:  'chinese'
+  },
+  {
+    name:         'Francesco',
+    address:      '1090, Wien',
+    phone_number: '+43 3547',
+    category:  'italian'
+  },
+  {
+    name:         'Le Bol',
+    address:      '1010, Wien',
+    phone_number: '+43 7958',
+    category:  'french'
+  },
+  {
+    name:         'Belgian Choc',
+    address:      '1230, Wien',
+    phone_number: '+43 54676',
+    category:  'belgian'
+  },
+    {
+    name:         'Koinonia',
+    address:      '1190, Wien',
+    phone_number: '+43 3452345',
+    category:  'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
