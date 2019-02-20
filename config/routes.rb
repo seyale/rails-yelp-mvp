ada70d192cbb4f0ce492e34640acb2b167c89e6d
Alexander Seyss <a@seyss.at>
2019-02-20 17:45:11 +0000

    Layout adaptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :restaurants
